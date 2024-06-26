3e6aa616381c2c8f4cfec8c0d1a8affc7447801a
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-06-26 23:10:28 +0900

    Call by Value/Reference 예제 만듬

---
+++ src/week1/Day3/PrimitiveParamEx.java
@@ -0,0 +1,24 @@
+package src.week1.Day3;
+
+import javax.xml.crypto.Data;
+
+class Data1{
+    int x;
+}
+
+public class PrimitiveParamEx {
+    public static void main(String[] args) {
+        Data1 d = new Data1();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+        change(d.x);
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x);
+    }
+
+    static void change(int x){
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+}
