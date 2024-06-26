ce073101538bb8a1d74030be05797eea3759b79e
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-06-26 15:46:00 +0900

    Day1, Day2 실습 코드 추가

---
+++ src/week1/Day1/ex/HelloElice.java
@@ -0,0 +1,8 @@
+package src.week1.Day1.ex;
+
+public class HelloElice {
+    public static void main(String[] args) {
+        // 지시사항을 참고하여 코드를 작성하세요.
+        System.out.println("Hello, Elice!");
+    }
+}
---
+++ src/week1/Day2/ex/LoanScreening.java
@@ -0,0 +1,25 @@
+package week1.Day2.ex;
+
+import java.util.Scanner;
+
+public class LoanScreening {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 지시사항 1번을 참고하여 코드를 작성하세요.
+        boolean isAdult = scanner.nextBoolean();
+        // 지시사항 2번을 참고하여 코드를 작성하세요.
+        double monthlyIncome = scanner.nextInt();
+        // 지시사항 3번을 참고하여 코드를 작성하세요.
+        int creditScore = scanner.nextInt();
+        // 지시사항 4번을 참고해 주세요.
+        System.out.println("성인 여부: " + isAdult);
+        System.out.println("월 수입이 기준치 이상인가요? " + (monthlyIncome >= 170));
+        System.out.println("신용 점수가 기준치 이상인가요? " + (creditScore >= 650));
+
+        // 대출 승인 가능 여부를 계산합니다.
+        boolean isApproved = (isAdult && monthlyIncome >= 170 && creditScore >= 650);
+
+        System.out.println("대출 승인 가능 여부: " + isApproved);
+    }
+}
