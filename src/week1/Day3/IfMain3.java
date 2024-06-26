9fb48235a1b375f0fadd8be9ae357567dd5698ca
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-06-26 17:40:49 +0900

    Day3 이론코드 추가

---
+++ src/week1/Day3/IfMain3.java
@@ -0,0 +1,25 @@
+package src.week1.Day3;
+
+public class IfMain3 {
+    public static void main(String[] args) {
+        int age = 10;
+
+        if(age >= 19){
+            System.out.println("성인");
+        }else if(age < 13){
+            System.out.println("어린이");
+        }else{
+            System.out.println("청소년");
+        }
+
+        /*
+        if(조건식){
+            //1. 조건식이 참인 경우 실행
+        } else if(조건식2){
+            //2. 조건식이 참인 경우 실행
+        } else{
+            //3. 앞의 조건에 해당하지 않는 모든 경우 실행
+        }
+         */
+    }
+}
