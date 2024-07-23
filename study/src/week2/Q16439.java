2e44b3d3f17a8e6fd25d433e3942c5e4d314c909
[CLOUD4] 송진욱 <wlsdnr1233@naver.com>
2024-07-23 18:02:50 +0900

    study week2 Q16439 commit

--- study/src/week2/Q16439.java
+++ study/src/week2/Q16439.java
@@ -1,20 +1,25 @@
 package week2;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Q16439 {
+    static int N;
+    static int M;
+    static int[][] like;
+    static boolean[] visit;
+    static int max;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[][] like = new int[N][M];
+        like = new int[N][M];
+        visit = new boolean[M];
 
+        // 입력 받은 값 like 배열에 저장
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < M; j++) {
@@ -22,9 +27,37 @@ public class Q16439 {
             }
         }
 
-        int[] top = new int[3];
+        dfs(0,0);
+        System.out.println(max);
+    }
 
+    static void dfs(int start, int depth){
+        //3개의 치킨을 고른 상태
+        if(depth == 3){
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                int temp = 0;
+                for (int j = 0; j < M; j++) {
+                    //visit[j]가 true면 고른 치킨
+                    if(visit[j]){
+                        temp = Math.max(temp, like[i][j]);
+                    }
+                }
+                sum += temp;
+            }
+            //sum과 max를 비교해서 최댓값 도출
+            max = Math.max(sum, max);
+            return;
+        }
 
-
+        for (int i = start; i < M; i++) {
+            //해당 값을 방문하지 않았다면
+            if(!visit[i]){
+                visit[i] = true; // 해당 값을 방문상태로 변경
+                dfs(i + 1, depth + 1); // 다음 자식 값 방문을 위해 depth 1 증가시키면서 재귀 돌림
+                //자식값 방문 끝나고 돌아오면 방문값을 방문하지 않은 상태로 변경
+                visit[i] = false;
+            }
+        }
     }
 }
